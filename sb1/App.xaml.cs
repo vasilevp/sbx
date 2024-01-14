a63986c6a22f34ce1a4d44596142b1dee6e393fc
Pavel Vasilev <explosere@gmail.com>
2024-01-14 04:25:21 +0400

    Add exception handler

--- sb1/App.xaml.cs
+++ sb1/App.xaml.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 
 namespace sb1
 {
@@ -13,5 +7,14 @@ namespace sb1
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            this.DispatcherUnhandledException += (sender, e) =>
+            {
+                MessageBox.Show(e.Exception.Message + $"\n{e.Exception.StackTrace}", $"Unhandled {e.GetType().Name}");
+
+                e.Handled = true;
+            };
+        }
     }
 }
